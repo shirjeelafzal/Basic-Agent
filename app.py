383f0ac178eb47cae820d7794494a40a73dda71b
shirjeelafzal <shirjeelafzals@gmail.com>
2024-11-13 13:00:00 +0500

    add conditional nodes in the langgraph

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from typing import Annotated, Literal, TypedDict
+
+from langchain_core.messages import HumanMessage
+from langchain_anthropic import ChatAnthropic
+from langchain_core.tools import tool
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, START, StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode
 
 from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -17,17 +25,74 @@ model = ChatGroq(model="llama3-8b-8192")
 search = TavilySearchResults(max_results=2)
 tools = [search]
 
-system_message = SystemMessage(
-    content="Only use tools for questions requiring specific information or data lookup. "
-            "For simple introductions or casual conversation, respond directly without any tool."
+# system_message = SystemMessage(
+#     content="Only use tools for questions requiring specific information or data lookup. "
+#             "For simple introductions or casual conversation, respond directly without any tool."
+# )
+# memory = MemorySaver()
+# agent_executor = create_react_agent(model, tools,checkpointer=memory)
+# config = {"configurable": {"thread_id": "abc123"}}
+
+# for chunk in agent_executor.stream(
+#     {"messages": [system_message,HumanMessage(content="What is the whether in SF?")]}, config
+# ):
+#     print(chunk)
+#     print("----")
+
+tool_node = ToolNode(tools)
+
+
+def should_continue(state: MessagesState) -> Literal["tools", END]:
+    messages = state['messages']
+    last_message = messages[-1]
+    # If the LLM makes a tool call, then we route to the "tools" node
+    if last_message.tool_calls:
+        return "tools"
+    # Otherwise, we stop (reply to the user)
+    return END
+
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    messages = state['messages']
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+workflow = StateGraph(MessagesState)
+
+workflow.add_node("agent", call_model)
+workflow.add_node("tools", tool_node)
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.add_edge(START, "agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
 )
-memory = MemorySaver()
-agent_executor = create_react_agent(model, tools,checkpointer=memory)
-config = {"configurable": {"thread_id": "abc123"}}
 
-for chunk in agent_executor.stream(
-    {"messages": [system_message,HumanMessage(content="What is the whether in SF?")]}, config
-):
-    print(chunk)
-    print("----")
 
+
+workflow.add_edge("tools", 'agent')
+
+# Initialize memory to persist state between graph runs
+checkpointer = MemorySaver()
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable.
+# Note that we're (optionally) passing the memory when compiling the graph
+app = workflow.compile(checkpointer=checkpointer)
+
+# Use the Runnable
+final_state = app.invoke(
+    {"messages": [HumanMessage(content="what is the weather in sf")]},
+    config={"configurable": {"thread_id": 42}}
+)
+print(final_state["messages"][-1].content)
